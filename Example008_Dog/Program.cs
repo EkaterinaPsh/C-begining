9305a1533c50c180fcadd78fcfbcaeb37a6315a5
Ekaterina <kate071299@gmail.com>
2022-11-29 12:56:30 +1000

    Example008 Dog

---
+++ Example008_Dog/Program.cs
@@ -0,0 +1,22 @@
+﻿int count = 0, distance = 10000, firstFriendSpeed = 1, secondFriendSpeed = 2, dogSpeed = 5, friend = 2, time;
+
+while (distance > 10)
+{
+  if (friend == 1)
+  {
+    time = distance / (firstFriendSpeed + dogSpeed);
+    friend = 2;
+  }
+  else
+  {
+    time = distance / (secondFriendSpeed + dogSpeed);
+    friend = 1;
+
+  }
+
+  distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+  count = count + 1;
+}
+
+Console.WriteLine("Собака пробежала " + count + " раз");
+
